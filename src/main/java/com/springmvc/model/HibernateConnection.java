490f10abce19d950daeacdd898eedd04d3b2534a
Kongpop Janjongkam <kookong111@gmail.com>
2025-11-30 11:17:19 +0700

    Update HibernateConnection.java

--- src/main/java/com/springmvc/model/HibernateConnection.java
+++ src/main/java/com/springmvc/model/HibernateConnection.java
@@ -17,12 +17,12 @@ public class HibernateConnection {
 		String dockerProfile = System.getProperty("spring.profiles.active");
 		if ("docker".equals(dockerProfile)) {
 			// Docker environment - connect to mysql container
-			url = "jdbc:mysql://mysql:3306/project_shabu2?characterEncoding=UTF-8&useSSL=false&serverTimezone=UTC";
+			url = "jdbc:mysql://mysql:3307/project_shabu2?characterEncoding=UTF-8&useSSL=false&serverTimezone=UTC";
 			uname = "root";
 			pwd = "1234";
 		} else {
 			// Local development environment
-			url = "jdbc:mysql://localhost:3306/project_shabu2?characterEncoding=UTF-8";
+			url = "jdbc:mysql://localhost:3307/project_shabu2?characterEncoding=UTF-8";
 			uname = "root";
 			pwd = "1234";
 		}
