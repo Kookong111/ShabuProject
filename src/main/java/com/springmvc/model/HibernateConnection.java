a4bcefb2d8b0b88a01047c4c4774fbda955ab89d
Kookong111 <Kookong111>
2025-12-04 14:41:45 +0700

    Hibernate

--- src/main/java/com/springmvc/model/HibernateConnection.java
+++ src/main/java/com/springmvc/model/HibernateConnection.java
@@ -17,12 +17,12 @@ public class HibernateConnection {
 		String dockerProfile = System.getProperty("spring.profiles.active");
 		if ("docker".equals(dockerProfile)) {
 			// Docker environment - connect to mysql container
-			url = "jdbc:mysql://mysql:3307/project_shabu2?characterEncoding=UTF-8&useSSL=false&serverTimezone=UTC";
+			url = "jdbc:mysql://mysql:3306/project_shabu2?characterEncoding=UTF-8&useSSL=false&serverTimezone=UTC";
 			uname = "root";
 			pwd = "1234";
 		} else {
 			// Local development environment
-			url = "jdbc:mysql://localhost:3307/project_shabu2?characterEncoding=UTF-8";
+			url = "jdbc:mysql://localhost:3306/project_shabu2?characterEncoding=UTF-8";
 			uname = "root";
 			pwd = "1234";
 		}
@@ -30,33 +30,30 @@ public class HibernateConnection {
 
 	public static SessionFactory doHibernateConnection(){
 		if (sessionFactory == null) {
-			
-		
-		Properties database = new Properties();
-		database.setProperty("hibernate.hbm2ddl.auto", "update"); // หลังจากสร้างตารางแล้วให้เอาออก
-		database.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
-		database.setProperty("hibernate.connection.username", uname);
-		database.setProperty("hibernate.connection.password", pwd);
-		database.setProperty("hibernate.connection.url", url);
-
-		database.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
-		Configuration cfg = new Configuration()
-				.setProperties(database)
-				.addPackage("com.springmvc.model")
-				.addAnnotatedClass(Order.class)
-				.addAnnotatedClass(Tables.class)
-				.addAnnotatedClass(Payment.class)
-				.addAnnotatedClass(MenuFood.class)
-				.addAnnotatedClass(OrderDetail.class)
-				.addAnnotatedClass(Reserve.class)
-				.addAnnotatedClass(Customer.class)
-				.addAnnotatedClass(Employee.class)
-				.addAnnotatedClass(Manager.class)
-				.addAnnotatedClass(FoodType.class)
-				;
-
-		StandardServiceRegistryBuilder ssrb = new StandardServiceRegistryBuilder().applySettings(cfg.getProperties());
-		sessionFactory = cfg.buildSessionFactory(ssrb.build());
+			Properties database = new Properties();
+			database.setProperty("hibernate.hbm2ddl.auto", "update"); // หลังจากสร้างตารางแล้วให้เอาออก
+			database.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
+			database.setProperty("hibernate.connection.username", uname);
+			database.setProperty("hibernate.connection.password", pwd);
+			database.setProperty("hibernate.connection.url", url);
+			database.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
+			Configuration cfg = new Configuration()
+					.setProperties(database)
+					.addPackage("com.springmvc.model")
+					.addAnnotatedClass(Order.class)
+					.addAnnotatedClass(Tables.class)
+					.addAnnotatedClass(Payment.class)
+					.addAnnotatedClass(MenuFood.class)
+					.addAnnotatedClass(OrderDetail.class)
+					.addAnnotatedClass(Reserve.class)
+					.addAnnotatedClass(Customer.class)
+					.addAnnotatedClass(Employee.class)
+					.addAnnotatedClass(Manager.class)
+					.addAnnotatedClass(FoodType.class)
+					;
+	
+			StandardServiceRegistryBuilder ssrb = new StandardServiceRegistryBuilder().applySettings(cfg.getProperties());
+			sessionFactory = cfg.buildSessionFactory(ssrb.build());
 		}
 		return sessionFactory;
 	}
